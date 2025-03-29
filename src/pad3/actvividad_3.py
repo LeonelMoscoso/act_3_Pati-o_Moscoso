2aaa67d9c2fb77288b4592b56243f3d75d72e13d
Patino_Moscoso <leonel.moscoso@est.iudigital.edu.co>
2025-03-29 18:58:43 -0500

    punto 2

--- src/pad3/actvividad_3.py
+++ src/pad3/actvividad_3.py
@@ -17,56 +17,20 @@ df = pd.DataFrame(data)
 # Guardar el DataFrame en un archivo CSV
 df.to_csv('punto_1.csv', index=False)
 
-
-# Convertir a DataFrame
-df = pd.DataFrame(data)
-
-# Mostrar el DataFrame
-print(df)
-# Guardar el DataFrame en un archivo CSV
-df.to_csv('frutas.csv', index=False)
-
 # Crear el DataFrame
 data = {
     'Granadilla': [20, 49],
     'Tomates': [50, 100]
 }
-
-# Definir los índices
-index = ['ventas 2021', 'ventas 2022']
-
 # Crear el DataFrame
-ventas_frutas = pd.DataFrame(data, index=index)
+df = pd.DataFrame(data, index=['ventas 2021', 'ventas 2022'])
 
-# Mostrar el DataFrame
-print(ventas_frutas)
-# Datos
-categorias = ['Granadilla', 'Tomates']
-ventas_2021 = [20, 50]
-ventas_2022 = [49, 100]
+# Guardar el DataFrame en un archivo CSV
+df.to_csv('punto_2_ventas.csv')
 
-# Posiciones de las barras
-x = np.arange(len(categorias))
+print("Archivo 'ventas.csv' creado exitosamente.")
 
-# Ancho de las barras
-ancho = 0.35
 
-# Crear el gráfico de barras
-fig, ax = plt.subplots()
-barras1 = ax.bar(x - ancho/2, ventas_2021, ancho, label='Ventas 2021')
-barras2 = ax.bar(x + ancho/2, ventas_2022, ancho, label='Ventas 2022')
-
-# Añadir etiquetas y título
-ax.set_ylabel('Cantidad de Ventas')
-ax.set_title('Ventas de Granadilla y Tomates por Año')
-ax.set_xticks(x)
-ax.set_xticklabels(categorias)
-ax.legend()
-
-# Mostrar el gráfico
-plt.tight_layout()
-plt.savefig('ventas_granadilla_tomates.png')  # Guardar en archivo
-plt.show()
 
 # Crear la serie con los utensilios y sus respectivas unidades
 utensilios = pd.Series(
